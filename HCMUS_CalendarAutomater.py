dcec5a1dccb1481eda9b2ea2943666e5961f623e
レジカイ <ledinhhai1312@gmail.com>
2019-08-31 17:40:47 +0700

    Added cookie option

--- HCMUS_CalendarAutomater.py
+++ HCMUS_CalendarAutomater.py
@@ -1,7 +1,17 @@
 import crawlHTML
 import postCalendar
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--cookie', dest = 'cookie', default = 'none')
+args = parser.parse_args()
+
 def main():
-    crawlHTML.getScheduleJson(InputPath = 'TKB.html', OutputPath = 'TKB.json')
+    if(args.cookie == 'none'):
+        crawlHTML.getScheduleJson()
+    else:
+        crawlHTML.getScheduleJsonWithCookie(Cookie = args.cookie)
     postCalendar.postCalendar(postCalendar.readJSON('TKB.json'))
+
 if __name__ == '__main__':
     main()
